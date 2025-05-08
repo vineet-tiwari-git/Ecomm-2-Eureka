1a2072e356aecf1b2ed4133fa271d8bf7d4552a0
Vineet Tiwari <tiwari_vineet@hotmail.com>
2025-05-08 21:43:26 +0530

    commit

--- src/main/java/com/vineet/eureka/DemoSpringEurekaServiceApplication.java
+++ src/main/java/com/vineet/eureka/DemoSpringEurekaServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+//This annotation is used to enable the Eureka server functionality in the Spring Boot application.
 public class DemoSpringEurekaServiceApplication {
 
 	public static void main(String[] args) {
